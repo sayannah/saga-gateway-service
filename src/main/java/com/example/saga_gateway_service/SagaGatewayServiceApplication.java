92d7988e71582a688262fbb6e620e919a86de505
sayannah <sayannah620@gmail.com>
2026-05-07 09:56:42 +0400

    Initial commit

---
+++ src/main/java/com/example/saga_gateway_service/SagaGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.saga_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saga_gateway_service/SagaGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saga_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
